11963937491ab44476ef32d377e4bd07b648df24
Wenyu Zhang <zachzwy@gmail.com>
2020-01-07 18:09:41 -0800

    Add real sponsor (#3)
    * add real sponsor
    
    * Sponsor handle small resolution
    
    * Update css

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
 function Sponsors() {
-    return (<div className="text-center">
+    const sponsorList = [
+        {
+            name: "twilio",
+            src: "//logo.clearbit.com/twilio.org",
+            link: "https://www.twilio.com/"
+        },
+        {
+            name: "stripe",
+            src: "//logo.clearbit.com/stripe.com",
+            link: "https://stripe.com/"
+        },
+        {
+            name: "lightbend",
+            src: "//logo.clearbit.com/lightbend.com",
+            link: "https://www.lightbend.com/"
+        }
+    ]
+
+    return (<div className="text-center sponsor">
         <h1>SPONSORS</h1>
         <div>
             <div className="grid-wrapper">
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
-                <div className="grid-element"><i className="fa fa-twitter" aria-hidden="true"/></div>
+                {sponsorList.map((sponsor, i) => (
+                    <div key={i} className="grid-element">
+                        <a href={sponsor.link} rel="noopener noreferrer" target="_blank">
+                            <img src={`${sponsor.src}?size=100`} alt={`${sponsor.name}'s logo`} />
+                        </a>
+                    </div>
+                ))}
             </div>
         </div>
     </div>);
